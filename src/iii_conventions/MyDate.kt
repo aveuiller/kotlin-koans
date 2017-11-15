e81a126c9d25f64002f8b21090a8d3e673286c19
Antoine Veuiller <aveuiller@gmail.com>
2017-11-15 17:43:12 +0100

    section 3

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,18 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import iv_properties.toMillis
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return toMillis().compareTo(other.toMillis())
+    }
+
+    operator fun plus(interval: TimeInterval): MyDate = addTimeIntervals(interval, 1)
+    operator fun plus(range: TimeRange): MyDate = addTimeIntervals(range.timeInterval, range.number)
+    operator fun inc(): MyDate = nextDay()
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, endInclusive = other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +20,23 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(number: Int): TimeRange = TimeRange(this, number)
+
+data class TimeRange(val timeInterval: TimeInterval, val number: Int)
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator() = object : Iterator<MyDate> {
+        private var current = start
+
+        override fun hasNext(): Boolean = current <= endInclusive
+
+        override fun next(): MyDate {
+            return current++
+        }
+    }
+
+    operator fun contains(date: MyDate): Boolean {
+        val millis = date.toMillis()
+        return millis >= start.toMillis() && millis <= endInclusive.toMillis()
+    }
+}
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,20 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private val instances: Int = 0) {
+    operator fun invoke(): Invokable = Invokable(instances + 1)
+
+    fun getNumberOfInvocations() = instances
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
